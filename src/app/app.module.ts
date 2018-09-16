35b4be0460e24ccd60236f6355fe23e3b8199eaa
James Kan <james0077721@hotmail.com>
2018-09-15 17:17:15 -0700

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecordComponent } from './record/record.component';
+import { MapComponent } from './map/map.component';
+import { ItemComponent } from './item/item.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecordComponent,
+    MapComponent,
+    ItemComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule
